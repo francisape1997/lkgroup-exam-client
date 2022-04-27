4d7e02c0ce0f8b934c7d2926d587d09a22883317
Francis <90559251+francisape1997@users.noreply.github.com>
2022-04-27 15:52:00 +0800

    Add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,48 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+// Layout
+import MainLayout from '../layouts/MainLayout.vue';
+
+// Pages
+import Players from '../views/players/index.vue';
+import PlayerForm from '../views/players/form.vue';
+
+const routes = [
+  { path: '/:pathMatch(.*)*', name: 'Not Found', component: () => import('../views/error/404/index.vue') },
+
+  // Players
+  {
+      path: '/',
+      component: MainLayout,
+      children:
+      [
+          {
+              path: 'players',
+              component: Players,
+          },
+          {
+              path: 'player/create',
+              component: PlayerForm,
+              meta:
+              {
+                  page: 'create',
+              },
+          },
+          {
+              path: 'player/:id/edit',
+              component: PlayerForm,
+          },
+          {
+            path: '/',
+            redirect: '/players',
+        }
+      ],
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  routes,
+});
 
-export default router
+export default router;
